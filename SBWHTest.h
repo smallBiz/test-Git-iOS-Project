ea542d18b92f2592c9e298235f0c5de87e590265
Ian Wilson <apple@smallbusinesswebservices.co.uk>
2014-09-27 09:50:48 +0100

    added a test class

---
+++ SBWHTest.h
@@ -0,0 +1,13 @@
+//
+//  SBWHTest.h
+//  Git Test
+//
+//  Created by Ian Wilson on 27/09/2014.
+//  Copyright (c) 2014 SBWS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SBWHTest : NSObject
+
+@end
---
+++ SBWHTest.m
@@ -0,0 +1,13 @@
+//
+//  SBWHTest.m
+//  Git Test
+//
+//  Created by Ian Wilson on 27/09/2014.
+//  Copyright (c) 2014 SBWS. All rights reserved.
+//
+
+#import "SBWHTest.h"
+
+@implementation SBWHTest
+
+@end
